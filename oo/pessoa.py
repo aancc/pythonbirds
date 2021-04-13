73ecd90c8a467b3853f47964fc6650b513586799
augusto <augusto_zy@hotmail.com>
2021-04-13 06:43:29 -0300

    Criado atributos de instância

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,17 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.idade = idade
+        self.nome = None
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
     p = Pessoa()
-    print(Pessoa.cumprimentar(p,))
+    print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Augusto'
+    print(p.nome)
+    print(p.idade)
